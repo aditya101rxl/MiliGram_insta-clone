0b1a93cbbc0a5736f323239a6124f9bbcbf5f350
Deepak Mishra <70760888+Depgit@users.noreply.github.com>
2021-03-11 11:33:30 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ export const auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decodedData = jwt.verify(token, SECRET_KEY)
-        req.userId = decodedData?.id;
+        req.userId = decodedData.id;// decodedData?.id won't run
         next()
     } catch (error) {
         console.log(error);
